4ec469272a73b2984e5f05d2afe02fa15bc68bee
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-02-05 14:09:59 +0700

    feat: start build auth sign in for both student and admin page, also absence page

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+
 import { AppModule } from "./app.module";
 
+const localAddress = "127.0.0.1";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  /**
+   * @see https://docs.nestjs.com/techniques/performance
+   */
+  try {
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+
+    app.enableCors({ origin: "*" });
+
+    await app.listen(5000, localAddress);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 bootstrap();
